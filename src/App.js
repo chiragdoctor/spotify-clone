f7abee7ecb54925ab52ad913c3b83260e49f99c3
chiragdoctor <chirag.doctor@gmail.com>
2020-08-07 16:37:19 +0100

    Set access token & Get User imformation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
 import React, { useEffect, useState } from 'react';
+import SpotifyWebAPi from 'spotify-web-api-js';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
+import Player from './Player';
+
+const spotify = new SpotifyWebAPi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -10,15 +14,19 @@ function App() {
   // run code based on given condition
   useEffect(() => {
     const hash = getTokenFromUrl();
+    // Clearning the url which has access_token after redirection.
+    window.location.hash = '';
     const _token = hash.access_token;
     if (_token) {
       setToken(_token);
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        console.log('👱', user);
+      });
     }
   }, []);
 
-  return (
-    <div className='app'>{token ? <h1>I am logged In</h1> : <Login />}</div>
-  );
+  return <div className='app'>{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
